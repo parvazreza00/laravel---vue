4acf30bb465c6c4bccbf31f62baf3b83ae96b43f
parvazreza00 <parvazreza00@gmail.com>
2022-08-22 08:08:14 +0600

    Laravel and Vuejs Authentication by BreezeAuth

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,24 @@
+<script setup>
+import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue';
+import { Head } from '@inertiajs/inertia-vue3';
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <BreezeAuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Hi! 
+            </h2>
+        </template>
+
+        <div class="py-2">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                
+                    <h1 class="text-success fs-4">Hello! I love laravel and vuejs</h1>
+               
+            </div>
+        </div>
+    </BreezeAuthenticatedLayout>
+</template>
